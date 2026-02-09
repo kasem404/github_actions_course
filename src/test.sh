9c4973dd919903af12f5a0762ad6f006275f943c
Kassem Mohamed <Qassem123197>
2026-02-09 16:34:49 +0200

    ffffffffffffsssss

--- src/test.sh
+++ src/test.sh
@@ -1,10 +1,13 @@
-EXPECTED = " hello, test "
-OUTPUT = $(node -e "console.log(requere('./src/app')('test'))")
-if ["$OUTPUT" == "$EXPECTED"];
-echo "test password"
-exit 0
-else
-echo "test failed"
-exit 1
-fi
+#!/bin/bash
 
+EXPECTED="hello, test"
+
+OUTPUT=$(node -e "console.log(require('./src/app')('test'))")
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+  echo "test passed"
+  exit 0
+else
+  echo "test failed"
+  exit 1
+fi
